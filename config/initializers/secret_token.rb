8ba272f215d0648845193b0b1fd6f02763dac786
Cynthia Kiser <cnk@ugcs.caltech.edu>
2011-12-22 00:30:29 -0800

    newly generated app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cms::Application.config.secret_token = 'eac24d16b256687101b198d31cb6bed6d7ede15d28f91819a959b74ddb28890bd7e7c2f01d78af447fd8d84d52af11841e08869cad0aa20c422135defe96acf7'
